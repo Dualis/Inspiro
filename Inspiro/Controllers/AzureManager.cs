b9a150fdd581b57d70fc77356483c103db8d97e0
Jonathan Miller <jjwm@clear.net.nz>
2016-11-28 20:37:41 +1300

    Added Data Models for SQL

---
+++ Inspiro/Controllers/AzureManager.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inspiro.Controllers
+{
+    public class AzureManager
+    {
+
+        private static AzureManager instance;
+        private MobileServiceClient client;
+
+        private AzureManager()
+        {
+            this.client = new MobileServiceClient("MOBILE_APP_URL");
+        }
+
+        public MobileServiceClient AzureClient
+        {
+            get { return client; }
+        }
+
+        public static AzureManager AzureManagerInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AzureManager();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
